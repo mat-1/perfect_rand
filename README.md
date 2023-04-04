b9c816efd07997058409e451675f78779b29303c
mat <github@matdoes.dev>
2023-04-04 18:40:43 +0000

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+A port of the Blackrock cipher used in [Masscan](https://github.com/robertdavidgraham/masscan) to Rust.
+
+It's primarily meant for efficiently randomizing the order of port scans, without having to put every possible target in memory and shuffling.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A port of the Blackrock cipher used in masscan to Rust.
+//! A port of the Blackrock cipher used in [Masscan](https://github.com/robertdavidgraham/masscan) to Rust.
 //!
 //! It's primarily meant for efficiently randomizing the order of port scans,
 //! without having to put every possible target in memory and shuffling.
